0d6028f71cb9946e02d2141ee0adb7bb27254391
GakTau24 <107782664+GakTau24@users.noreply.github.com>
2022-11-18 23:27:28 +0700

    Add files via upload

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,31 @@
+import videoBg from "../assets/background.mp4";
+import Animeted from "../assets/animated.gif";
+import "./profile.css"
+import { Link, Route, Routes } from "react-router-dom";
+
+export function Profile () {
+    return (
+        <div className="container">
+            <video src={videoBg} autoPlay loop muted/>
+            <div className="logo">
+            <div className="overlay">
+                <img src={Animeted} alt="" />
+            </div>
+            </div>
+            <div className="nama">
+                <h3 className="animate__animated animate__zoomIn animate__infinite">GakTau</h3>
+            </div>
+            <div className="game">
+            <p className="animate__animated animate__bounceInLeft animate__delay-1s">★ Minecraft</p>
+            <p className="animate__animated animate__bounceInLeft animate__delay-2s">★ Valorant</p>
+            <p className="animate__animated animate__bounceInLeft animate__delay-3s">★ Pubg Mobile</p>
+            <p className="animate__animated animate__bounceInLeft animate__delay-4s">★ Point Blank</p>
+            </div>
+
+            <div className="home">
+                <p><Link to="/">Home</Link></p>
+            </div>
+        </div>
+        
+    )
+}
